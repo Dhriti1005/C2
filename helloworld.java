22d273fcdd860f590e6010fddd490a9a77d95f39
Dhriti1005 <makamdhriti@gmail.com>
2024-10-12 19:02:20 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+public class helloworld {
+    public static void main(String[] args){
+        int a= 5;
+        int b= 10;
+        int sum= a+b;
+        int diff= a-b;
+        int mult= a*b;
+        int div= a/b;
+        System.out.println("addition of a&b"+sum);
+        System.out.println("subtraction of a&b"+diff);
+        System.out.println("multiplication of a&b"+mult);
+        System.out.println("divition of a&b"+div);
+    }
+}
